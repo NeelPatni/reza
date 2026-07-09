8643e5f8772a2dac981b1fa0c7ca96adb894e4ee
Neel Patni <neelpatni14@gmail.com>
2026-07-09 17:15:05 +0530

    server.js

--- server.js
+++ server.js
@@ -1,71 +1,13 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const dotenv = require("dotenv");
-dotenv.config();
-const dns = require("dns");
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
-dns.setDefaultResultOrder("ipv4first");
-
-
-dotenv.config();
 
 const app = express();
 
-// Middleware
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-
-
-// Database Connection
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("✅ MongoDB Connected");
-  })
-  .catch((err) => {
-    console.log("❌ MongoDB Connection Error:", err);
-  });
-
-// Routes
-const authRoutes = require("./routes/authRoutes");
-const userRoutes = require("./routes/UserRoutes");
-const productRoutes = require("./routes/productRoutes");
-const orderRoutes = require("./routes/orderRoutes");
-const dashboardRoutes = require("./routes/dashboardRoutes");
-const categoryRoutes = require("./routes/categoryRoutes");
-
-// const uploadRoutes = require("./routes/uploadRoutes");
-
-// Middleware API
-
-app.use("/api/auth", authRoutes);
-app.use("/api/categories", categoryRoutes);
-app.use("/api/products", productRoutes);
-
-// app.use("/api/upload", uploadRoutes);
-
-app.use("/api/orders", orderRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/dashboard", dashboardRoutes);
-app.use(
-  "/uploads",
-  express.static(
-    "uploads"
-  )
-);
-
-
-
-// Test Route
 app.get("/", (req, res) => {
-  res.send("Admin Panel Backend Running 🚀");
+  res.send("Backend Working 🚀");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
+  console.log("Server Started on", PORT);
 });
