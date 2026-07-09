51c3cada8fc92522a05ec166d19e90e090fcdd41
Neel Patni <neelpatni14@gmail.com>
2026-07-09 17:24:15 +0530

    test 4

--- server.js
+++ server.js
@@ -8,21 +8,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("✅ MongoDB Connected");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+mongoose.connect(process.env.MONGO_URI);
+
+console.log("Loading authRoutes...");
+
+const authRoutes = require("./routes/authRoutes");
+
+console.log("authRoutes Loaded");
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Step 3 Working");
+  res.send("Step 4");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("✅ STEP 3 SUCCESS");
+  console.log("STEP 4 SUCCESS");
 });
