c2ac3b3fe818687cbbc817a96efc238c0e6264e2
Neel Patni <neelpatni14@gmail.com>
2026-07-09 17:47:22 +0530

    test 5 again-1

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,64 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  getAllUsers,
-  getSingleUser,
-  updateUserRole,
-  blockUser,
-  unblockUser,
-  deleteUser,
-} = require("../controllers/userController");
-
-const auth = require("../middleware/auth");
-const admin = require("../middleware/admin");
-
-// Get All Users
-router.get(
-  "/",
-  auth,
-  admin,
-  getAllUsers
-);
-
-// Get Single User
-router.get(
-  "/:id",
-  auth,
-  admin,
-  getSingleUser
-);
-
-// Update User Role
-router.put(
-  "/:id/role",
-  auth,
-  admin,
-  updateUserRole
-);
-
-// Block User
-router.put(
-  "/:id/block",
-  auth,
-  admin,
-  blockUser
-);
-
-// Unblock User
-router.put(
-  "/:id/unblock",
-  auth,
-  admin,
-  unblockUser
-);
-
-// Delete User
-router.delete(
-  "/:id",
-  auth,
-  admin,
-  deleteUser
-);
+router.get("/", (req, res) => {
+  res.json({ message: "Users Working" });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch(err => console.error(err));
 
 console.log("Loading UserRoutes");
 
