d4ba68fe1602698cdf9953e2aa3ee7751ab6c463
Neel Patni <neelpatni14@gmail.com>
2026-07-09 17:36:28 +0530

    test 9

--- server.js
+++ server.js
@@ -10,20 +10,20 @@ app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI);
 
-console.log("Loading orderRoutes");
+console.log("Loading dashboardRoutes");
 
-const orderRoutes = require("./routes/orderRoutes");
+const dashboardRoutes = require("./routes/dashboardRoutes");
 
-console.log("orderRoutes Loaded");
+console.log("dashboardRoutes Loaded");
 
-app.use("/api/orders", orderRoutes);
+app.use("/api/dashboard", dashboardRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Step 8");
+  res.send("Step 9");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("STEP 8 SUCCESS");
+  console.log("STEP 9 SUCCESS");
 });
