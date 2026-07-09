f5cd0aa98762ddec8f098d1f969cc7684b6bc1e4
Neel Patni <neelpatni14@gmail.com>
2026-07-09 17:22:28 +0530

    test 3

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
-  res.send("Step 2 Working ✅");
+  res.send("Step 3 Working");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("✅ STEP 2 SUCCESS");
+  console.log("✅ STEP 3 SUCCESS");
 });
